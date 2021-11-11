25ab278dfa4ad16f8ec596bec38e50105841737b
ninamitevska <nina.mitevska@gmail.com>
2021-11-11 22:31:18 +0100

    New component is made Language.js and also directory for components...part2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import FlashcardList from "./FlashcardList";
 import Languages from "./Language";
 
-const App = () => {
+function App() {
 
     return (
         <div className="App">
